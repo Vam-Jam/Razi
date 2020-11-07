3dbbad1386cb4d42911af0b5667a3425b803daad
Vamist <Vamist@protonmail.com>
2020-11-07 12:49:10 +0000

    Finished

--- src/commands/admins.rs
+++ src/commands/admins.rs
@@ -1,7 +1,10 @@
 use crate::settings::toml::*;
 use crate::RAZI_CONFIG;
 use serenity::{
-    framework::standard::{macros::command, CommandResult},
+    framework::standard::{
+        macros::{check, command},
+        Args, CheckResult, CommandOptions, CommandResult,
+    },
     model::channel::Message,
     prelude::Context,
 };
@@ -11,16 +14,51 @@ use std::process::Command;
 #[command]
 #[help_available]
 #[aliases("emergency_do_not_use")]
-#[description("This is a command you are only suppose to use in an emergency.\nDO NOT USE THIS IF THIS IS NOT THE CASE")]
-#[allowed_roles("361262801387520000", "474289311206866944", "364040665149079564")]
+#[description("This is a command you are only suppose to use in an emergency.\nLike when razi start's to come alive or something dumb.\n////DO NOT USE THIS IF THIS IS NOT THE CASE////")]
+#[checks("ADMIN")]
 pub async fn emergency(ctx: &Context, msg: &Message) -> CommandResult {
+    match msg
+        .reply(
+            &ctx.http,
+            "Well, you did the command you probably shouldn't have done. Goodbye",
+        )
+        .await
+    {
+        Err(err) => {
+            println!("Couldnt send reply message => {}", err);
+            return Ok(()); // return because they didnt get to see the message >:(
+        }
+        _ => (),
+    }
+
+    // TODO: Uninstall Razi
+
+    Command::new("cargo")
+        .current_dir(current_dir().unwrap())
+        .arg("clean")
+        .spawn()
+        .expect("Luckily died on cargo clean");
+
+    Command::new("shutdown")
+        .arg("now")
+        .spawn()
+        .expect("Luckily died on shutdown");
+    Ok(())
+}
+
+#[check]
+#[name = "ADMIN"]
+async fn owner_check(
+    ctx: &Context,
+    msg: &Message,
+    _: &mut Args,
+    _: &CommandOptions,
+) -> CheckResult {
     let mut config = RaziConfig::new();
 
     RAZI_CONFIG.with(|cell| {
         config = cell.borrow().clone();
     });
-
-    let mut is_admin = false;
 
     for admin_list in config.discord.admin_roles {
         match msg
@@ -30,50 +68,16 @@ pub async fn emergency(ctx: &Context, msg: &Message) -> CommandResult {
         {
             Ok(result) => {
                 if result {
-                    is_admin = true;
-                    break;
+                    return true.into();
                 }
             }
-            Err(err) => println!("Error with has_role check => {}", err), // maybe return instead of checking other roles?
+            _ => (),
         };
     }
 
-    if is_admin {
-        match msg
-            .reply(
-                ctx,
-                "Well, you did the command you probably shouldn't have done. Goodbye",
-            )
-            .await
-        {
-            Err(err) => {
-                println!("Couldnt send reply message => {}", err);
-                return Ok(()); // return because they didnt get to see the message >:(
-            }
-            _ => (),
-        }
-
-        // TODO: Uninstall Razi
-
-        Command::new("cargo")
-            .current_dir(current_dir().unwrap())
-            .arg("clean")
-            .spawn()
-            .expect("Luckily died on cargo clean");
-
-        Command::new("shutdown")
-            .arg("now")
-            .spawn()
-            .expect("Luckily died on shutdown");
-    } else {
-        match msg
-            .reply(ctx, "Sorry but you don't have the perms to do that.")
-            .await
-        {
-            Err(err) => println!("Couldnt send reply message => {}", err),
-            _ => (),
-        }
+    if msg.author.id == 142300409002852352 {
+        return true.into();
     }
 
-    Ok(())
+    false.into()
 }
